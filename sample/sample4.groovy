7fb5a3bd9d02700f6ece202fe53428f671c52e9d
UEHARA Junji <uehaj@jggug.org>
2009-07-05 22:29:05 +0900

    change file names

--- sample/sample4.groovy
+++
@@ -1,9 +0,0 @@
-// JFrame sample
-def bx = new LispBuilder()
-
-bx.build{progn
-         ${setq; x; $(new javax.swing.JFrame())}
-         ${setq; pane; ${call; x; $"getContentPane"}}
-         ${call; pane; $"add"; $(new javax.swing.JLabel("hello"))}
-         ${call; x; $"show"}
-}.eval()
